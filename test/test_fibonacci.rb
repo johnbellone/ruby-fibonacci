afaaa63a1234d33e4970fde988c9a215a7d6793f
John Bellone <jb@thunkbrightly.com>
2011-10-26 22:23:36 -0400

    Tests for fib

--- test/test_fibonacci.rb
+++ test/test_fibonacci.rb
@@ -1,7 +1,41 @@
 require 'helper'
 
 class TestFibonacci < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+
+  should "0 return 0" do
+    0.closest_fibonacci === 0
   end
+
+  should "1 return 1" do
+    1.closest_fibonacci === 1
+  end
+
+  should "2 return 1" do
+    2.closest_fibonacci === 1
+  end
+
+  should "3 return 2" do
+    3.closest_fibonacci === 2
+  end
+
+  should "4 return 3" do
+    4.closest_fibonacci === 3
+  end
+
+  should "5 return 5" do
+    5.closest_fibonacci === 5
+  end
+
+  should "6 return 8" do
+    6.closest_fibonacci === 8
+  end    
+
+  should "-2 not return -1" do
+    -2.closest_fibonacci != -1
+  end    
+
+  should "-1 return 1" do
+    -1.closest_fibonacci === 1
+  end        
+  
 end
